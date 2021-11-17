98504cc8328a4ccc54935cdc71d3fb5946859317
Eduardo913 <cesarvilla903@gmail.com>
2021-11-17 12:03:33 -0600

    CORS

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = process.env.PORT || 3000
 
 app.set('port', port);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 var server = app.listen(app.get('port'), () => {
     console.log("Servidor  escuchando en el puerto %s", app.get('port'))
 })
