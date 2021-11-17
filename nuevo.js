b30a8e3985e586173d978a27807aa0cf76eb0752
Eduardo913 <cesarvilla903@gmail.com>
2021-11-16 23:05:04 -0600

    changes

---
+++ nuevo.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var http = require('http').Server(app);
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ server: http });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+wss.on('error', function(error) {
+    console.log(error);
+});
+
+wss.on('connection', function connection(ws) {
+    var location = url.parse(ws.upgradeReq.url, true);
+    // you might use location.query.access_token to authenticate or share sessions
+    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    console.log('connected');
+    ws.on('message', function incoming(message) {
+        console.log(message);
+        wss.clients.forEach(function each(client) {
+            client.send(JSON.stringify(message));
+        });
+    });
+    //ws.send('something');
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
