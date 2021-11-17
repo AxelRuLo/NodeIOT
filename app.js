4b10f19387fb5986f04f5bdf7451f3b85cf901d7
Eduardo913 <cesarvilla903@gmail.com>
2021-11-16 19:06:32 -0600

    socket

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/",(req,res)=>{
-    res.json({"respuesta":"todo salio bien"})
+var server = require('http').createServer(app)
+const io = require("socket.io")(server)
+
+//sockets
+
+io.on('connection', (cliente) => {
+    console.log("cliente conectado" + cliente.id)
+
+    cliente.on('message', (data) => {
+        console.log("datos: " + data)
+    })
 })
 
-app.listen(port,()=>{
- console.log("iniciando la app")
+
+//express
+app.get("/", (req, res) => {
+    res.json({ "respuesta": "todo salio bien" })
+})
+
+app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto %s", port)
 })
