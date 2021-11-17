a66cd92c16a0e0c1ed2c483f164cf233db85e134
Eduardo913 <cesarvilla903@gmail.com>
2021-11-17 00:18:20 -0600

    new changes

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ io.on('connection', (socket) => {
     // })
 
     socket.on('disconnect', () => {
-            console.log("el socket se desconecto " + socket.id)
+        console.log("el socket se desconecto " + socket.id)
+    })
+    socket.on('message', (data) => {
+            console.log("el socket dice" + data)
         })
         //     // cliente.emit('message', 'Bienvenido!')
 })
